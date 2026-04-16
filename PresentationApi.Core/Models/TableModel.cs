75a6f3a7f06698dfbe1bc5ecf0764cece3fbe404
Artem Zubkov <17artem12@gmail.com>
2026-04-16 13:15:37 +0500

    add table

---
+++ PresentationApi.Core/Models/TableModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PresentationCreator.Models;
+
+public class TableModel
+{
+    public long X { get; set; }
+    public long Y { get; set; }
+    public long width { get; set; }
+    public long height { get; set; }
+    public List<List<TextTableModel>> data { get; set; }
+}
--- PresentationApi.Core/Models/TextModel.cs
+++ PresentationApi.Core/Models/TextModel.cs
@@ -11,3 +11,13 @@ public class TextModel
     public long width { get; set; }
     public long height { get; set; }
 }
+
+public class TextTableModel
+{
+    public string value { get; set; }
+    public int fontSize { get; set; }
+    public string fontFamily { get; set; }
+    public string fontColor { get; set; }
+    public string center { get; set; }
+    public string  backgroundColor { get; set; }
+}
